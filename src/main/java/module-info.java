ae6f3dbefd37dd566c0cc3f940947731aa473c23
Daniel Perano <51095634+danielperano@users.noreply.github.com>
2024-12-14 14:11:35 -0800

    Add module-info.java

---
+++ src/main/java/module-info.java
@@ -0,0 +1,10 @@
+module hummingbird {
+    exports myworld.hummingbird;
+    exports myworld.hummingbird.assembler;
+    exports myworld.hummingbird.instructions;
+    exports myworld.hummingbird.instructions.arithmetic;
+    exports myworld.hummingbird.instructions.bitwise;
+    exports myworld.hummingbird.instructions.flow;
+    exports myworld.hummingbird.instructions.memory;
+    exports myworld.hummingbird.instructions.string;
+}
